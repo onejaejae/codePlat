b4838d4bb4c8a69395419674147351ce4ad16fae
Cho Won Jae <yoteamo7@naver.com>
2021-03-03 18:24:55 +0900

    배포 준비 #5

--- Server/db.js
+++ Server/db.js
@@ -4,17 +4,12 @@ import mongoose from "mongoose";
 dotenv.config();
 
 mongoose
-  .connect(
-    process.env.NODE_ENV === "production"
-      ? process.env.mongoURI
-      : process.env.DB_HOST,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(process.env.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("Mongoose Connected"))
   .catch((err) => console.log(err));
 
