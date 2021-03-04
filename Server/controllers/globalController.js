923fb25d09f96f720458958d6a042bcc2b44fe1e
Cho Won Jae <yoteamo7@naver.com>
2021-03-04 13:46:35 +0900

    globalController file path->location 수정

--- Server/controllers/globalController.js
+++ Server/controllers/globalController.js
@@ -118,7 +118,7 @@ export const postJoinOption = async (req, res) => {
     if (files.length > 0) {
       // eslint-disable-next-line prefer-destructuring
       const length = files.length;
-      update.avatarUrl = files[length - 1].path;
+      update.avatarUrl = files[length - 1].location;
     }
 
     try {
